dac2f00c32bfeae6db8f0cffff4967dcd3323145
ixora-0 <39961970+ixora-0@users.noreply.github.com>
2024-04-17 16:37:40 -0400

    fix(sound): Properly enable pipewire.

--- modules/nixos/sound.nix
+++ modules/nixos/sound.nix
@@ -1,6 +1,6 @@
-{
+{ pkgs, ... }: {
   # enable sound with pipewire
-  sound.enable = true;
+  sound.enable = false;  # only meant for ALSA-based configurations
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;  # rtkit is optional but recommended
   services.pipewire = {
@@ -11,5 +11,16 @@
     # If you want to use JACK applications, uncomment this
     # jack.enable = true;
   };
-
+  # bluetooth support: https://nixos.wiki/wiki/PipeWire
+  # NOTE: only on 24.05
+  services.pipewire.wireplumber.configPackages = [
+    (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
+      bluez_monitor.properties = {
+        ["bluez5.enable-sbc-xq"] = true,
+        ["bluez5.enable-msbc"] = true,
+        ["bluez5.enable-hw-volume"] = true,
+        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+      }
+    '')
+  ];
 }
