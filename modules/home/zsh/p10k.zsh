90eaa7893f4c79d9453cb0aae476d149ddb86ad4
ixora-0 <ixora-0@users.noreply.github.com>
2024-04-18 17:39:27 +0000

    style: minify p10k.zsh

--- modules/home/zsh/p10k.zsh
+++ modules/home/zsh/p10k.zsh
@@ -1,16 +1,14 @@
-# comment 1
 'builtin' 'local' '-a' 'p10k_config_opts'
 [[ ! -o 'aliases'         ]] || p10k_config_opts+=('aliases')
 [[ ! -o 'sh_glob'         ]] || p10k_config_opts+=('sh_glob')
 [[ ! -o 'no_brace_expand' ]] || p10k_config_opts+=('no_brace_expand')
 'builtin' 'setopt' 'no_aliases' 'no_sh_glob' 'brace_expand'
 () {
-  # comment 2
   emulate -L zsh -o extended_glob
   unset -m '(POWERLEVEL9K_*|DEFAULT_USER)~POWERLEVEL9K_GITSTATUS_DIR'
   [[ $ZSH_VERSION == (5.<1->*|<6->.*) ]] || return
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
-    os_icon  # comment 3
+    os_icon
     dir
     vcs
     newline
