67b6811b2dd005280e1d2a454e80c00e06e9ff9b
ixora-0 <39961970+ixora-0@users.noreply.github.com>
2024-04-19 11:51:31 -0400

    discord: Add vesktop option.

--- modules/home/discord.nix
+++ modules/home/discord.nix
@@ -1,4 +1,7 @@
-{ pkgs, ... }: let
+{ pkgs, lib, config, ... }: let
+  cfg = config.modules.discord;
+  mkIfElse = import ../../helpers/mkIfElse.nix { inherit lib; };
+
   catppuccinThemesSrc = pkgs.fetchgit {
     url = "https://github.com/catppuccin/discord";
     sparseCheckout = ["themes"];
@@ -8,11 +11,22 @@ in
 {
   # TODO: declarative ~/.config/Vencord/setting/settings.json
   # maybe see https://github.com/FlafyDev/nixos-config/commit/5cd0781bae16fed8007513c24434890909c1a680
-  xdg.configFile."Vencord/themes".source = catppuccinThemesSrc;
-  home.packages = [
-    (pkgs.discord.override {
-      withOpenASAR = true;
-      withVencord = true;
-    })
+  options.modules.discord.vesktop.enable = lib.mkEnableOption ''
+    Whether to install vesktop instead of dicord with vencord.
+    Good if want to share screen properly in wayland.
+  '';
+
+  config.xdg.configFile."Vencord/themes".source = catppuccinThemesSrc;
+  config.home.packages = with pkgs; [
+    (mkIfElse cfg.vesktop.enable
+      (vesktop.override {
+        withSystemVencord = false;  # letting vesktop manage it's own version
+                                    # which fixes the no vencord section in settings issue
+      })
+      (discord.override {
+        withOpenASAR = true;
+        withVencord = true;
+      })
+    )
   ];
 }
