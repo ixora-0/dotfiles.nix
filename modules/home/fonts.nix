178ed953c3ab4daec7ee0165caeb99bb6d09efca
ixora-0 <39961970+ixora-0@users.noreply.github.com>
2024-04-30 19:53:56 -0400

    refactor(fonts): move fonts installation from nixos to home

--- modules/home/fonts.nix
+++ modules/home/fonts.nix
@@ -1,6 +1,17 @@
 { pkgs, ... }: {
   fonts.fontconfig.enable = true;
   home.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+
+    (iosevka.override {
+      set = "Iris";
+      privateBuildPlan = builtins.readFile ../nixos/default-fonts/private-build-plans.toml;
+    })
+    (nerdfonts.override {
+      fonts = ["NerdFontsSymbolsOnly"];
+    })
     corefonts
     vistafonts
   ];
