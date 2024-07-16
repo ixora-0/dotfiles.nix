5cd8d8fe564350a6861cffb1610cee303dbbc42e
ixora-0 <39961970+ixora-0@users.noreply.github.com>
2024-07-15 20:05:10 -0400

    ollama: use ollama from unstable

--- modules/nixos/ollama.nix
+++ modules/nixos/ollama.nix
@@ -1,6 +1,7 @@
-{
+{pkgs-unstable, ...}: {
   services.ollama = {
     enable = true;
+    package = pkgs-unstable.ollama;
     acceleration = "cuda";
     listenAddress = "0.0.0.0";
     # models = ""  # getting an error saying can't set models directory
