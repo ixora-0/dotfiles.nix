9bdf0ee1708fd8bd0826a02794006246be99f427
ixora-0 <39961970+ixora-0@users.noreply.github.com>
2024-08-07 02:29:52 -0400

    open-webui: remove unecessary data volume

--- modules/nixos/open-webui.nix
+++ modules/nixos/open-webui.nix
@@ -6,9 +6,4 @@
       open-webui = import ../../containers/open-webui.nix;
     };
   };
-  system.activationScripts = {
-    script.text = ''
-      install -d -m 755 /home/ixora/open-webui/data -o root -g root
-    '';
-  };
 }
