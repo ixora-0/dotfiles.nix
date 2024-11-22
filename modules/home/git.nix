cfec056aa03545355002f9023b5e0d618e873658
ixora-0 <39961970+ixora-0@users.noreply.github.com>
2024-11-22 14:08:37 -0500

    git: Switch to ssh signing.

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -6,7 +6,8 @@
     userEmail = "39961970+ixora-0@users.noreply.github.com";
 
     signing.signByDefault = true;
-    signing.key = "0959B5BC39B44064";
+    # signing.key = "0959B5BC39B44064";
+    signing.key = "~/.ssh/id_ed25519.pub";
 
     # use delta pager
     delta.enable = true;
@@ -18,6 +19,7 @@
     extraConfig = {
       core.autocrlf = "input";
       init.defaultBranch = "main";
+      gpg.format = "ssh";
     };
 
     ignores = [
