bc9a5ee56e1680def9985131e3a3d31ddaa7ab55
ixora-0 <39961970+ixora-0@users.noreply.github.com>
2024-12-12 00:18:32 -0500

    sops: Downgrade flake.

--- flake.nix
+++ flake.nix
@@ -26,8 +26,8 @@
 
     nixos-wsl.url = "github:nix-community/NixOS-WSL";
 
-    sops-nix.url = "github:Mic92/sops-nix";
     lanzaboote.url = "github:nix-community/lanzaboote/v0.4.1";
+    sops-nix.url = "github:Mic92/sops-nix/e9b5eef9b51cdf966c76143e13a9476725b2f760";
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
     ags.url = "github:Aylur/ags";
     astal.url = "github:Aylur/astal";
