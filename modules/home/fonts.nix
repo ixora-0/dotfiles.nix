0548a39673997a75da6fc9344af385250e37d461
ixora-0 <39961970+ixora-0@users.noreply.github.com>
2025-03-08 16:38:43 -0500

    fonts: More fonts.

--- modules/home/fonts.nix
+++ modules/home/fonts.nix
@@ -1,16 +1,30 @@
 { pkgs, ... }: {
   fonts.fontconfig.enable = true;
   home.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-emoji
-
+    # core
+    font-manager
+    noto-fonts noto-fonts-cjk-sans noto-fonts-emoji
     (iosevka.override {
       set = "Iris";
       privateBuildPlan = builtins.readFile ../nixos/default-fonts/private-build-plans.toml;
     })
-    (iosevka-bin.override { variant = "Aile"; })
     (nerdfonts.override { fonts = ["NerdFontsSymbolsOnly"]; })
     corefonts vistafonts
+
+    # sets
+    ibm-plex
+
+    # serifs
+    roboto-slab
+    merriweather
+    crimson-pro
+    (iosevka-bin.override { variant = "Etoile"; })
+    source-serif-pro
+    libre-baskerville
+
+    # sans
+    merriweather-sans
+    (iosevka-bin.override { variant = "Aile"; })
+    source-sans
   ];
 }
