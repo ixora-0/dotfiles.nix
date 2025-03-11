824d65f9b24704ca26061fe6dc0534bd808c10e7
ixora-0 <39961970+ixora-0@users.noreply.github.com>
2025-03-11 18:59:38 -0400

    yazi: Works now.

--- modules/home/yazi.nix
+++ modules/home/yazi.nix
@@ -1,18 +1,6 @@
 {
   programs.yazi = {
     enable = true;
-    # enableZshIntegration = true;  # doesn't work with zoxide
-                                    # https://github.com/sxyazi/yazi/issues/978
+    enableZshIntegration = true;
   };
-  # HACK: set custom zsh integration
-  programs.zsh.initExtra = ''
-    function ya() {
-      local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
-      yazi "$@" --cwd-file="$tmp"
-      if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-        cd "$cwd"
-      fi
-      rm -f -- "$tmp"
-    }
-  '';
 }
