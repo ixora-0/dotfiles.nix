cf65e8769c5bd0846baae9ebdb8df41853e7a49e
ixora-0 <39961970+ixora-0@users.noreply.github.com>
2025-08-29 18:36:05 -0400

    nvidia: Add battery saver boot profile.

--- hosts/nerine/nvidia.nix
+++ hosts/nerine/nvidia.nix
@@ -1,4 +1,4 @@
-{config, ... }: {
+{ config, lib, ... }: {
   hardware.nvidia = {
     modesetting.enable = true;  # required
 
@@ -27,4 +27,44 @@
   # load nvidia driver for xorg and wayland
   services.xserver.videoDrivers = ["nvidia"];
 
+  specialisation =  {
+    battery-saver.configuration = {
+      system.nixos.tags = [ "battery-saver" ];
+
+      boot.extraModprobeConfig = ''
+        blacklist nouveau
+        options nouveau modeset=0
+      '';
+
+      services.udev.extraRules = ''
+        # Remove NVIDIA USB xHCI Host Controller devices, if present
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+
+        # Remove NVIDIA USB Type-C UCSI devices, if present
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+
+        # Remove NVIDIA Audio devices, if present
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+
+        # Remove NVIDIA VGA/3D controller devices
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+      '';
+      boot.blacklistedKernelModules = [
+        "nouveau"
+        "nvidia"
+        "nvidia_drm"
+        "nvidia_modeset"
+      ];
+
+      hardware.nvidia = {
+        prime.sync.enable = lib.mkForce false;
+        prime.offload.enable = lib.mkForce false;
+        prime.offload.enableOffloadCmd = lib.mkForce false;
+        powerManagement = {
+          enable = lib.mkForce false;
+          finegrained = lib.mkForce false;
+        };
+      };
+    };
+  };
 }
