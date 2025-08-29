66d476ab1611f1ffdab53402f8bc9884f72e6e84
ixora-0 <39961970+ixora-0@users.noreply.github.com>
2025-08-29 18:32:16 -0400

    steam: Move to nixos module.

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -1,6 +1,7 @@
 {
   programs.gamemode.enable = true;
   programs.gamemode.settings.general.inhibit_screensaver = 0;
+  programs.steam.enable = true;
 
   users.users.ixora.extraGroups = ["gamemode"];
 }
