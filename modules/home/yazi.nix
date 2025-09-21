e7cfce7d0e1feed357b09c99c1ec8e89b5c192cd
ixora-0 <39961970+ixora-0@users.noreply.github.com>
2025-09-21 15:00:23 -0400

    yazi: Add dragon drag and drop.

--- modules/home/yazi.nix
+++ modules/home/yazi.nix
@@ -1,6 +1,10 @@
-{
+{ pkgs, ... }: {
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;
+    keymap.mgr.prepend_keymap = [{
+      on = ["<C-n>"];
+      run = ''shell -- ${pkgs.dragon-drop}/bin/dragon-drop -x -i -T "$1"'';
+    }];
   };
 }
