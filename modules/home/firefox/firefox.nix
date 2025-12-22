be8dd12f99adf448c185b357cae776ff65670acf
ixora-0 <39961970+ixora-0@users.noreply.github.com>
2025-12-21 23:48:55 -0500

    firefox: Add profile.

--- modules/home/firefox/firefox.nix
+++ modules/home/firefox/firefox.nix
@@ -4,6 +4,7 @@
   else
     ./userChrome-Linux.css
   );
+
   betterfox = pkgs.fetchFromGitHub {
     owner = "yokoffing";
     repo = "Betterfox";
@@ -11,9 +12,28 @@
     sha256 = "sha256-zGpfQk2gY6ifxIk1fvCk5g5SIFo+o8RItmw3Yt3AeCg=";
   };
   userJS = (builtins.readFile "${betterfox}/user.js") + (builtins.readFile ./betterfox_extra.js);
-in {
-  programs.firefox.enable = true;
-  programs.firefox.profiles."nixOS-profile" = {
+
+  # NOTE: have to enable extensions manually at first launch
+  # NOTE: have to configure toolbar manually
+  # NOTE: extensions that are not available for installation from here:
+  # https://addons.mozilla.org/en-US/firefox/addon/catppuccin-gh-file-explorer/
+  extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+    ublock-origin             # ad blocker
+    bitwarden                 # password manager
+    sidebery                  # tree style tab
+                              # NOTE: have to import ./sidebery/sideberySettings manually
+    simple-translate          # quick translate
+    undoclosetabbutton        # actually undo close tab
+
+    betterttv                 # twitch/yt emotes
+    return-youtube-dislikes   # third party yt dislikes
+    videospeed                # yt speed controller
+    stylus                    # custom site styles
+                              # NOTE: have to import https://github.com/catppuccin/userstyles/releases/download/all-userstyles-export/import.json manually
+    darkreader
+    # refined-github
+  ];
+  default-config = {
     userChrome = userChromeCSS;
     extraConfig = userJS;
     settings = {
@@ -22,38 +42,13 @@ in {
       "browser.tabs.inTitlebar" = 0;  # disables title bar
       "browser.startup.page" = 3;     # reopen last closed tabs
     };
-
-    # NOTE: have to enable extensions manually at first launch
-    # NOTE: have to configure toolbar manually
-    # NOTE: extensions that are not available for installation from here:
-    # https://addons.mozilla.org/en-US/firefox/addon/catppuccin-gh-file-explorer/
-    extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
-      ublock-origin             # ad blocker
-      bitwarden                 # password manager
-      sidebery                  # tree style tab
-                                # NOTE: have to import ./sidebery/sideberySettings manually
-      simple-translate          # quick translate
-      undoclosetabbutton        # actually undo close tab
-
-      betterttv                 # twitch/yt emotes
-      return-youtube-dislikes   # third party yt dislikes
-      videospeed                # yt speed controller
-      stylus                    # custom site styles
-                                # NOTE: have to import https://github.com/catppuccin/userstyles/releases/download/all-userstyles-export/import.json manually
-      darkreader
-      # refined-github
-    ];
-
-    # search engines
-    search.force = true;
-    search.default = "Startpage";
-    search.privateDefault = "Startpage";
-    search.order = ["Startpage" "google"];
-    search.engines."Startpage" = {
+  };
+  search-engines = {
+    "Startpage" = {
       urls = [{ template = "https://www.startpage.com/search?query={searchTerms}"; }];
       icon = "https://www.startpage.com/favicon.ico";
     };
-    search.engines."Nix Packages" = {
+    "Nix Packages" = {
       urls = [{
         template = "https://search.nixos.org/packages";
         params = [
@@ -64,60 +59,89 @@ in {
       icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
       definedAliases = [ "@np" ];
     };
-    search.engines."NixOS Wiki" = {
+    "NixOS Wiki" = {
       urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
       icon = "https://nixos.wiki/favicon.png";
       definedAliases = [ "@nw" ];
     };
-    search.engines."Home Manager - Option Search" = {
+    "Home Manager - Option Search" = {
       urls = [{
         template = "https://home-manager-options.extranix.com/?query={searchTerms}";
       }];
       icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
       definedAliases = [ "@hmo" ];
     };
-    search.engines."WordReference English definition" = {
+    "WordReference English definition" = {
       urls = [{ template = "https://www.wordreference.com/definition/{searchTerms}"; }];
       definedAliases = [ "@mean" ];
     };
-    search.engines."WordReference English synonyms" = {
+    "WordReference English synonyms" = {
       urls = [{ template = "https://www.wordreference.com/synonyms/{searchTerms}"; }];
       definedAliases = [ "@same" ];
     };
-    search.engines."Youtube" = {
+    "Youtube" = {
       urls = [{ template = "https://www.youtube.com/results?search_query={searchTerms}"; }];
       definedAliases = [ "@yt" ];
       icon = "https://www.svgrepo.com/show/13671/youtube.svg";
     };
-    search.engines."Wiktionary" = {
+    "Wiktionary" = {
       urls = [{ template = "https://en.wiktionary.org/wiki/{searchTerms}"; }];
       definedAliases = [ "@wt" ];
       icon = "https://upload.wikimedia.org/wikipedia/commons/8/83/En.wiktionary_favicon.svg";
     };
-    search.engines = {
-        bing.metaData.hidden = true;
-        google.metaData.alias = "@g"; # builtin engines only support specifying one additional alias 
-        wikipedia.metaData.alias = "@w";
-    };
+    bing.metaData.hidden = true;
+    google.metaData.alias = "@g"; # builtin engines only support specifying one additional alias
+    wikipedia.metaData.alias = "@w";
   };
+in {
+  programs.firefox.enable = true;
+  programs.firefox.profiles."nixOS-profile" = default-config // {
+    extensions.packages = extensions;
+    search.force = true;
+    search.default = "Startpage";
+    search.privateDefault = "Startpage";
+    search.order = ["Startpage" "google"];
+    search.engines = search-engines;
+  };
+
   programs.firefox.profiles."torproxy" = {
     id = 1;
     extraConfig = userJS;
     settings = {
-      "browser.ctrlTab.sortByRecentlyUsed" = true;
-      "browser.toolbars.bookmarks.visibility" = "never";
-      "browser.tabs.inTitlebar" = 0;  # disables title bar
-      "browser.startup.page" = 3;     # reopen last closed tabs
-
       "network.proxy.socks" = "127.0.0.1";
       "network.proxy.socks_port" = 8118;
       "network.proxy.type" = 1;
       "network.proxy.socks5_remote_dns" = true;
     };
-
     extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
-      ublock-origin             # ad blocker
+      ublock-origin
     ];
+  };
+
+  programs.firefox.profiles."nosuggest" = default-config // {
+    id = 2;
+    settings = {
+      # Disable search engine suggestions
+      "browser.search.suggest.enabled" = false;
+      "browser.urlbar.suggest.searches" = false;
+      # Disable history suggestions
+      "browser.urlbar.suggest.history" = false;
+      # Disable bookmark suggestions
+      "browser.urlbar.suggest.bookmark" = false;
+      # Disable top sites / shortcuts
+      "browser.urlbar.suggest.topsites" = false;
+      # Disable Firefox suggestions / sponsored content
+      "browser.urlbar.suggest.quicksuggest" = false;
+      "browser.urlbar.quicksuggest.enabled" = false;
+      "browser.urlbar.quicksuggest.sponsored" = false;
+      "browser.urlbar.quicksuggest.nonsponsored" = false;
+    };
+    extensions.packages = extensions;
+    search.force = true;
+    search.default = "Startpage";
+    search.privateDefault = "Startpage";
+    search.order = ["Startpage" "google"];
+    search.engines = search-engines;
   };
 
   xdg.mimeApps.defaultApplications = {
